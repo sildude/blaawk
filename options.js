c304efce57baf076ccc20aa499d93499033e5519
Silver Vaher <sildude@gmail.com>
2024-08-05 13:07:17 +0300

    update to v1.1, update styling, add toggle button

--- options.js
+++ options.js
@@ -1,7 +1,64 @@
+const seagullAffirmations = [
+  "Flap those wings! You're meant to soar above distractions.",
+  "Let's catch the thermal currents, not the time-wasters!",
+  'Feather by feather, build your nest of success.',
+  'Peck away at your goals, one small step at a time!',
+  "Spread your wings wide - there's a whole sky of possibilities out there!",
+  'Keep your beak pointed towards your dreams.',
+  "Squawk less, soar more - let's stay on task today!",
+  'Like a gull over the ocean, glide above the chaos with grace.',
+  'A smooth sea never made a skilled gull. Tackle those challenges!',
+  'Remember, every bird takes flight in its own time. Be patient with yourself.',
+  'Let your spirit soar high, above the mundane!',
+  `A true bird's eye view means focusing on what truly matters.`,
+  'Ruffle those feathers and shake off the distractions!',
+  'Catch the wind beneath your wings and rise above the noise.',
+  'Stay light on those webbed feet, ready to pivot towards your goals.',
+  'Just as we migrate with purpose, keep your journey focused and meaningful.',
+  "Nestle into your tasks today like they're part of your cozy nest.",
+  'Flutter towards productivity, and leave procrastination on the shoreline.',
+  'Keep your eyes sharp like a hawk on the prize ahead.',
+  "Whistle a happy tune as you glide through today's tasks effortlessly.",
+];
+
+function setAffirmation() {
+  document.getElementById('affirmations').innerText =
+    seagullAffirmations[Math.floor(Math.random() * seagullAffirmations.length)];
+}
+
+setAffirmation();
+
+document
+  .getElementById('affirmBlock')
+  .addEventListener('click', setAffirmation);
+
+const promptQuestions = [
+  'Are you sure you want to remove all rules?',
+  'Are you REALLY sure???',
+  'Think of all the missed productivity...',
+  'But... but... what about your goals?',
+  "Don't let those websites win!",
+  "You're stronger than this, I believe in you!",
+  "Seriously? You're gonna give up now?",
+  'Have you considered the existential implications of this decision?',
+  'Did you know that disabling this extension voids the warranty on your computer?',
+  'Is this because of something I said?',
+  "I'm not crying, you're crying.",
+  'Hold on, let me just check with the productivity gods... They said this is a bad idea.',
+  "Noooooo! Please don't go! I'm not gonna let you ruin your life!",
+  "Fine, but don't come crawling back when those websites take over your life.",
+];
+
 chrome.storage.local.get('blockedSites', ({blockedSites}) => {
   if (blockedSites) {
     document.getElementById('siteList').value = blockedSites.join('\n');
   }
+});
+
+chrome.storage.local.get('paused', ({paused}) => {
+  paused
+    ? (document.getElementById('toggleButton').checked = false)
+    : (document.getElementById('toggleButton').checked = true);
 });
 
 document.getElementById('save').addEventListener('click', () => {
@@ -9,6 +66,41 @@ document.getElementById('save').addEventListener('click', () => {
     .getElementById('siteList')
     .value.split('\n')
     .filter((site) => site.trim() !== '');
-  chrome.storage.local.set({blockedSites: siteList});
-  chrome.runtime.sendMessage({action: 'updateRules'});
+
+  chrome.storage.local.set({blockedSites: siteList}, () => {
+    chrome.runtime.sendMessage({
+      action: 'updateState',
+      paused: !document.getElementById('toggleButton').checked,
+    });
+  });
 });
+
+document.getElementById('toggleButton').addEventListener('change', (e) => {
+  if (!e.target.checked) {
+    askAllPrompts(promptQuestions).then((result) => {
+      if (result.result === 'ok') {
+        chrome.runtime.sendMessage({
+          action: 'updateState',
+          paused: true,
+        });
+      } else {
+        document.getElementById('toggleButton').checked = true;
+      }
+    });
+  } else {
+    chrome.runtime.sendMessage({
+      action: 'updateState',
+      paused: false,
+    });
+  }
+});
+
+async function askAllPrompts(prompts) {
+  for (const prompt of prompts) {
+    const userResponse = confirm(prompt);
+    if (!userResponse) {
+      return {result: 'cancel'};
+    }
+  }
+  return {result: 'ok'};
+}
--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,57 @@
 chrome.runtime.onMessage.addListener((message) => {
-  if (message.action === 'updateRules') {
-    updateRules();
-  }
+  if (message.action === 'updateState') togglePaused(message.paused);
 });
 
-function updateRules() {
-  chrome.storage.local.get('blockedSites', (result) => {
-    const blockedSites = result.blockedSites || [];
+function togglePaused(paused) {
+  chrome.storage.local.set({paused: paused});
+  if (paused) {
+    removeRules().then(() => {
+      console.info('Extension paused');
+    });
+  } else {
+    updateRules().then(() => {
+      console.info('Extension active');
+    });
+  }
+}
 
-    const rules = blockedSites.map((site, idx) => ({
-      id: idx + 1,
-      priority: 1,
-      action: {type: 'block'},
-      condition: {
-        urlFilter: site,
-        resourceTypes: ['main_frame'],
-      },
-    }));
+async function updateRules() {
+  try {
+    chrome.storage.local.get('blockedSites', async (res) => {
+      const blockedSites = res.blockedSites || [];
 
-    chrome.declarativeNetRequest
-      .updateDynamicRules({
-        removeRuleIds: blockedSites.map((_, index) => index + 1),
-      })
-      .then(() => {
-        chrome.declarativeNetRequest
-          .updateDynamicRules({
-            addRules: rules,
-          })
-          .then(() => {
-            console.info('Rules updated:', rules);
-          })
-          .catch((error) => {
-            console.error('Error adding rules:', error);
-          });
-      })
-      .catch((error) => {
-        console.error('Error removing rules:', error);
+      const rules = blockedSites.map((site, idx) => ({
+        id: idx + 1,
+        priority: 1,
+        action: {type: 'redirect', redirect: {extensionPath: '/blocked.html'}},
+        condition: {
+          urlFilter: site,
+          resourceTypes: ['main_frame'],
+        },
+      }));
+
+      await removeRules();
+
+      await chrome.declarativeNetRequest.updateDynamicRules({
+        addRules: rules,
       });
-  });
+
+      return {result: 'ok'};
+    });
+  } catch (error) {
+    console.error('Error adding rules:', error);
+    return {result: 'err', error};
+  }
+}
+
+async function removeRules() {
+  try {
+    const oldRules = await chrome.declarativeNetRequest.getDynamicRules();
+    const oldRuleIds = oldRules.map((rule) => rule.id);
+    return chrome.declarativeNetRequest.updateDynamicRules({
+      removeRuleIds: oldRuleIds,
+    });
+  } catch (error) {
+    console.error('Error removing rules:', error);
+  }
 }
